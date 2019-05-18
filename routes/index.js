a7cf70d1f0ed7417fc32c215985633abd06459df
E-matic <agbaezeemmanuel@gmail.com>
2019-05-18 09:34:45 +0100

    Refactored the code..

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,26 @@
 var express = require("express");
 var router = express.Router();
-// var timestamp = require('timestamp');
+
+function sendDateJSON(date, res) {
+  let unix = date.getTime();
+  let utc = date.toUTCString();
+  !unix ? res.json({ error: "invalid date" }) : res.json({ unix, utc });
+}
+
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Timestamp API" });
+  next();
 });
 
 router.get("/api/timestamp/:date", (req, res, next) => {
-  let x = req.params.date;
+  let x = req.params.date || new Date();
   let date = x == +x ? new Date(+x) : new Date(x);
-  let unix = date.getTime();
-  let utc = date.toUTCString();
-
-  !unix ? res.json({ error: "invalid date" }) : res.json({ unix, utc });
+  sendDateJSON(date, res);
+});
+router.get("/api/timestamp", (req, res, next) => {
+  let date = new Date();
+  sendDateJSON(date, res);
 });
 
 module.exports = router;
